a31376289c802923eed8b7de97c2801aefa4ed82
Jonathan E. Emmett <jon@emmettmedia.com>
2026-06-08 15:17:57 -0500

    moved from monorepo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from 'drizzle-kit';
 
 export default {
-	schema: './src/schema/index.ts',
-	out: './src/migrations',
+	schema: './lib/db/schema/index.ts',
+	out: './lib/db/migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL!,
